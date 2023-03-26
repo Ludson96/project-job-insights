2553d3bd4b9223764fb5198ad039dcb3d30a31c9
Ludson96 <ludson_ps27@hotmail.com>
2023-03-26 15:07:47 -0300

    test: :white_check_mark: add tests brazilian jobs

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    br_english = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    for job in br_english:
+        assert "title" in job
+        assert "salary" in job
+        assert "type" in job
+        assert "titulo" not in job
+        assert "salario" not in job
+        assert "tipo" not in job
